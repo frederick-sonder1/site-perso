1b741fc32c56bfdf2bc26ed3c5e893eaf9982774
[fredok]gitconfig --global user.email [fredericksonde@gmail.com] <frederick.sonder@laplateforme.io>
2022-03-27 13:14:45 +0200

    modif css responsive

--- script.js
+++ script.js
@@ -20,4 +20,102 @@ document.addEventListener('DOMContentLoaded',(Event)=>
         ulpt.style.display= 'none'
 
     })
+
+    //--------------------------- animation h1 [1]
+
+    var element = document.getElementById("watchme");
+    element.addEventListener("animationstart", listener, false);
+    element.addEventListener("animationend", listener, false);
+    element.addEventListener("animationiteration", listener, false);
+
+    element.className = "slidein";
+
+    function listener(event) {
+        // var l = document.createElement("li");
+        switch(event.type) {
+          case "animationstart":
+            // l.innerHTML = "Début : durée écoulée : " + event.elapsedTime;
+            break;
+          case "animationend":
+            // l.innerHTML = "Fin : durée écoulée : " + event.elapsedTime;
+            break;
+          case "animationiteration":
+            // l.innerHTML = "Nouvelle boucle démarrée à : " + event.elapsedTime;
+            break;
+        }
+        // document.getElementById("output").appendChild(l);
+      }
+
+       //--------------------------- animation h1 [2]
+
+       var element = document.getElementById("watchme2");
+       element.addEventListener("animationstart", listener, false);
+       element.addEventListener("animationend", listener, false);
+       element.addEventListener("animationiteration", listener, false);
+       
+       element.className = "slidein";
+       
+           function listener(event) {
+               // var l = document.createElement("li");
+               switch(event.type) {
+                 case "animationstart":
+                   // l.innerHTML = "Début : durée écoulée : " + event.elapsedTime;
+                   break;
+                 case "animationend":
+                   // l.innerHTML = "Fin : durée écoulée : " + event.elapsedTime;
+                   break;
+                 case "animationiteration":
+                   // l.innerHTML = "Nouvelle boucle démarrée à : " + event.elapsedTime;
+                   break;
+               }
+              //  document.getElementById("output").appendChild(l);
+             }
+        //--------------------------- animation h1 [3]
+
+        var element = document.getElementById("watchme3");
+        element.addEventListener("animationstart", listener, false);
+        element.addEventListener("animationend", listener, false);
+        element.addEventListener("animationiteration", listener, false);
+        
+        element.className = "slidein";
+        
+            function listener(event) {
+                // var l = document.createElement("li");
+                switch(event.type) {
+                  case "animationstart":
+                    // l.innerHTML = "Début : durée écoulée : " + event.elapsedTime;
+                    break;
+                  case "animationend":
+                    // l.innerHTML = "Fin : durée écoulée : " + event.elapsedTime;
+                    break;
+                  case "animationiteration":
+                    // l.innerHTML = "Nouvelle boucle démarrée à : " + event.elapsedTime;
+                    break;
+                }
+                // document.getElementById("output").appendChild(l);
+              }
+         //--------------------------- animation h1 [4]
+
+         var element = document.getElementById("watchme4");
+         element.addEventListener("animationstart", listener, false);
+        //  element.addEventListener("animationend", listener, false);
+         element.addEventListener("animationiteration", listener, false);
+         
+         element.className = "slidein";
+         
+             function listener(event) {
+                 // var l = document.createElement("li");
+                 switch(event.type) {
+                   case "animationstart":
+                     // l.innerHTML = "Début : durée écoulée : " + event.elapsedTime;
+                     break;
+                   case "animationend":
+                     // l.innerHTML = "Fin : durée écoulée : " + event.elapsedTime;
+                     break;
+                   case "animationiteration":
+                     // l.innerHTML = "Nouvelle boucle démarrée à : " + event.elapsedTime;
+                     break;
+                 }
+                //  document.getElementById("output").appendChild(l);
+               }
 })
